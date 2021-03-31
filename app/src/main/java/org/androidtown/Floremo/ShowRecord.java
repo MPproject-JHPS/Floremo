5a958d54fe30a5c7410c1eac701a1ff6deb961df
codingDJ <jul897813@gmail.com>
2021-03-31 21:58:19 +0900

    Basic files
    .xml and .java in empty activities

---
+++ app/src/main/java/org/androidtown/Floremo/ShowRecord.java
@@ -0,0 +1,14 @@
+package org.androidtown.Floremo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowRecord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_record);
+    }
+}
---
+++ app/src/main/java/org/androidtown/Floremo/addRecord.java
@@ -0,0 +1,14 @@
+package org.androidtown.Floremo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addRecord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_record);
+    }
+}
