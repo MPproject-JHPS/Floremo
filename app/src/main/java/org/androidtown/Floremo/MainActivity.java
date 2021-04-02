d564eb37035debc7f42d9cb4306393148d4b821b
codingDJ <jul897813@gmail.com>
2021-04-02 14:32:51 +0900

    Update MainActivity.java
    setContentView to drawerlayout since the logout button has been moved.

--- app/src/main/java/org/androidtown/Floremo/MainActivity.java
+++ app/src/main/java/org/androidtown/Floremo/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.drawerlayout);
 
         Button button = findViewById(R.id.logoutButton); //로그아웃 버튼
         button.setOnClickListener(new View.OnClickListener() {
