9da132ad38ee57e68af76a6b56b2bea1e4278cbb
codingDJ <jul897813@gmail.com>
2021-04-06 17:27:14 +0900

    Update DrawerActivity.java
    put logout funciton

--- app/src/main/java/org/androidtown/Floremo/DrawerActivity.java
+++ app/src/main/java/org/androidtown/Floremo/DrawerActivity.java
@@ -1,72 +1,60 @@
 package org.androidtown.Floremo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+
 public class DrawerActivity extends AppCompatActivity {
 
-    private DrawerLayout drawerLayout;
-    private View drawerView;
 
-    protected void onCreate (Bundle savedIntanceState) {
-        super.onCreate(savedIntanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
 
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
-        drawerView = (View)findViewById(R.id.drawer);
-
-        Button btn_open = (Button)findViewById(R.id.btn_open);
-        btn_open.setOnClickListener(new View.OnClickListener() {
+        TextView userName = findViewById(R.id.name);
+        Button button = findViewById(R.id.btn_logOut); //로그아웃 버튼
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                drawerLayout.openDrawer(drawerView);
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startJoinActivity(); //로그아웃되면 회원가입 화면으로 이동
             }
         });
 
-        Button btn_close = (Button)findViewById(R.id.btn_close);
-        btn_close.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                drawerLayout.closeDrawers();
-            }
-        });
+        Intent intent = getIntent();
+        String nickName = intent.getStringExtra("name"); //구글 로그인으로부터 닉네임 전달받음
+        userName.setText(nickName);
 
-        drawerLayout.setDrawerListener(listener);
-        drawerView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                return true;
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) //현재 로그인된 유저가 있는지 확인
+        {
+            startJoinActivity(); //로그인이 안되어 있으면 회원가입 화면으로 이동
+        } else { //현재 로그인 되어 있다면
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            if (user != null) {
+                for (UserInfo profile : user.getProviderData()) {
+                    // 사용자 이름 가져오기
+                    String name = profile.getDisplayName();
+                    userName.setText(name);
+                }
             }
-        });
+
+        }
     }
 
-    //해당 동작을 할 때 상태값을 받아 커스터마이징 할 수 있음
-
-    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
-        @Override
-        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
-
-        }
-
-        @Override
-        public void onDrawerOpened(@NonNull View drawerView) {
-
-        }
-
-        @Override
-        public void onDrawerClosed(@NonNull View drawerView) {
-
-        }
-
-        @Override
-        public void onDrawerStateChanged(int newState) {
-
-        }
-    };
+    private void startJoinActivity()
+    {
+        Intent intent = new Intent(this, JoinActivity.class);
+        startActivity(intent);
+    }
 }
