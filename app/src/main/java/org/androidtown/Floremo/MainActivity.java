89cce9895fe090fd7be46339016522a5dd195b77
seyeon2 <bb_bb_bb_53@naver.com>
2021-04-12 01:39:43 +0900

    update backward from main

--- app/src/main/java/org/androidtown/Floremo/MainActivity.java
+++ app/src/main/java/org/androidtown/Floremo/MainActivity.java
@@ -21,6 +21,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -33,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseUser mFirebaseUser;
     private DrawerLayout drawerLayout;
     private View drawerView;
+    private long backBtnTime = 0; // 뒤로가기 버튼 누를 때 필요
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -187,6 +189,21 @@ public class MainActivity extends AppCompatActivity {
 
         }
     };
+
+    @Override
+    public void onBackPressed() {
+        long curTime = System.currentTimeMillis();
+        long gapTime = curTime - backBtnTime;
+
+        if(0 <= gapTime && 2000 >= gapTime) {
+            super.onBackPressed();
+        }
+        else {
+            backBtnTime = curTime;
+            Toast.makeText(this, "한번 더 누르면 종료됩니다.",Toast.LENGTH_SHORT).show();
+        }
+
+    }
 
     private void startJoinActivity()
     {
