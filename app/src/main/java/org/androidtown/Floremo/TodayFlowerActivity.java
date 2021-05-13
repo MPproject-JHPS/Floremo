9692e22aad1d36cc9929d053d2363feec65ea150
codingDJ <jul897813@gmail.com>
2021-05-14 04:18:11 +0900

    Update TodayFlowerActicity
    go to ShowPolorideActivity.class

--- app/src/main/java/org/androidtown/Floremo/TodayFlowerActivity.java
+++ app/src/main/java/org/androidtown/Floremo/TodayFlowerActivity.java
@@ -3,8 +3,12 @@ package org.androidtown.Floremo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class TodayFlowerActivity extends AppCompatActivity {
 
@@ -13,10 +17,18 @@ public class TodayFlowerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_today_flower);
 
+        ImageButton bouquet = (ImageButton) findViewById(R.id.bouquet);
+
+        bouquet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TodayFlowerActivity.this, ShowPolorideActivity.class);
+                startActivity(intent);
+            }
+        });
 
         Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
 
 
