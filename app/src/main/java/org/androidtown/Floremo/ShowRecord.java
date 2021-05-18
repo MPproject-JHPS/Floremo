6b209728e624dfc57fec18662b322706ed854607
LetsGit-it <58305490+LetsGit-it@users.noreply.github.com>
2021-05-19 02:25:00 +0900

    re-push Change background color toolbar color

--- app/src/main/java/org/androidtown/Floremo/ShowRecord.java
+++ app/src/main/java/org/androidtown/Floremo/ShowRecord.java
@@ -107,6 +107,7 @@ public class ShowRecord extends AppCompatActivity implements RecordAdapter.OnMem
         Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mToolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
         mToolbar.setNavigationIcon(R.drawable.back);
+
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
     }
